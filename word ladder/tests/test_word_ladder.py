f42f72230559b9989e920e6ea42f33577e873baf
Abdur-Raheem-Lee <abdur-raheem@wethinkcode.co.za>
2025-04-09 15:28:28 +0200

    update: tests for word ladder

--- word ladder/tests/test_word_ladder.py
+++ word ladder/tests/test_word_ladder.py
@@ -6,12 +6,12 @@ class test_word_ladder(unittest.TestCase):
     
     def test_one(self):
         transformations = wl("hit", "cog", {"hot", "dot", "dog", "lot", "log", "cog"})
-        self.assertEqual(transformations, 5)
+        self.assertEqual(transformations, 4)
         
     
     def test_two(self):
         transformations = wl("hit", "dog", {"hot", "dot", "dog", "lot", "log", "cog"})
-        self.assertEqual(transformations, 4)
+        self.assertEqual(transformations, 3)
         
     
     def test_three(self):
@@ -21,3 +21,12 @@ class test_word_ladder(unittest.TestCase):
     def test_four(self):
         transformations = wl("hit", "hit", {"hit"})
         self.assertEqual(transformations, 1)
+        
+    def test_five(self):
+        transformations = wl("grape", "crate", {"grape", "graph", "crape", "crate"})
+        self.assertEqual(transformations, 3)
+        
+    def test_six(self):
+        transformations = wl("hit", "cog", {"hot", "dot", "dog", "cog"})
+        self.assertEqual(transformations, 4)
+        
